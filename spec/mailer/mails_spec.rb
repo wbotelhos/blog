3746ac2edd790a7cacf8f439ab09f8b4c019a94e
Washington Botelho <wbotelhos@gmail.com>
2012-06-09 20:18:17 -0300

    Comment e-mail tests.

--- spec/mailer/mails_spec.rb
+++ spec/mailer/mails_spec.rb
@@ -8,7 +8,7 @@ describe Mailer do
   let(:mailer)        { Mailer.comment(article, new_comment, comment) }
 
   context "configurations" do
-    it { mailer.should_not be_multipart }
+    it { mailer.should be_multipart }
 
     it "should queue email" do
       lambda { mailer.deliver }.should change(ActionMailer::Base.deliveries, :size).by(1)
@@ -18,18 +18,35 @@ describe Mailer do
 
   context "informations" do
     it { mailer.to.first.should eql(comment.email) }
-    it { mailer.from.first.should eql("noreply@wbotelhos.com.br") }
-    it { mailer.bcc.first.should eql("wbotelhos@gmail.com") }
+    it { mailer.from.first.should eql("noreply@#{CONFIG["url"]}") }
+    it { mailer.bcc.first.should eql(CONFIG["email"]) }
     it { mailer.subject.should eql("Artigo respondido") }
   end
 
-  context "content" do
-    it "should have the hello message" do
-      mail = mailer.deliver
+  context "content parts" do
+    it "should have the text body" do
+      body = mailer.parts.first.body
 
-      mail.body.should include("Olá #{comment.name},")
-      mail.body.should include(%[O article "#{article.title}" no qual você participou foi respondido por #{new_comment.name}.])
-      mail.body.should include(%[Não perca a discussão, visite: http://wbotelhos.com.br/articles/#{article.id}#comment-#{new_comment.id}])
+      body.should match(%r[<title>#{CONFIG["author"]}</title>])
+      body.should match(%r[<h1><a href="#{CONFIG['url_http']}" target="_blank" style="color: #333; font-size: 47px; letter-spacing: -3px; text-decoration: none; text-shadow: 2px 2px #FFF;">#{CONFIG["author"]}</a></h1>])
+      body.should match(%r[Ol&aacute;])
+      body.should match(%r[<strong>#{comment.name}</strong>,])
+      body.should match(%r[O artigo <strong><a href="#{CONFIG['url_http']}/articles/#{article.id}" style="color: #277DA8; text-decoration: none;">#{article.title}</a></strong> no qual voc&ecirc; participou foi respondido por <strong>#{new_comment.name}</strong>.])
+      body.should match(%r[N&atilde;o perca a discuss&atilde;o, visite: <a href="#{CONFIG['url_http']}/articles/#{article.id}#comment-#{new_comment.id}" style="color: #277DA8; text-decoration: none;">http://#{CONFIG["url"]}/articles/#{article.id}#comment-#{new_comment.id}</a>])
+      body.should match(%r[N&atilde;o perca a discuss&atilde;o, visite: <a href="#{CONFIG['url_http']}/articles/#{article.id}#comment-#{new_comment.id}" style="color: #277DA8; text-decoration: none;">http://#{CONFIG["url"]}/articles/#{article.id}#comment-#{new_comment.id}</a>])
+      body.should match(%r[<a href="#{CONFIG['url_http']}" target="_blank" style="color: #4080BF; text-decoration: none;">#{CONFIG['url_www']}</a><br />])
+      body.should match(%r[<div style="font-size: 11px;">#{CONFIG['description']}</div>])
+    end
+
+    it "should have the html body" do
+      body = mailer.parts.last.body
+
+      body.should include(%[Olá #{comment.name},])
+      body.should include(%[O artigo "#{article.title}" no qual você participou foi respondido por #{new_comment.name}.])
+      body.should include(%[Não perca a discussão, visite: #{CONFIG['url_http']}/articles/#{article.id}#comment-#{new_comment.id}])
+      body.should include(%[---])
+      body.should include(CONFIG['author'])
+      body.should include(CONFIG['url_http'])
     end
   end
 
