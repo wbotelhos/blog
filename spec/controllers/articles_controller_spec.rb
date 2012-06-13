62d34b2aadeaba4795c594dc52cb0b52f7203eb9
Washington Botelho <wbotelhos@gmail.com>
2012-06-13 01:05:29 -0300

    Take off hash argument.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -4,7 +4,7 @@ describe ArticlesController do
 
   describe ":search" do
     xit "should receives the params" do
-      controller.should_receive(:search).with(hash_including(:page => "10", :query => "some query"))
+      controller.should_receive(:search).with(:page => "10", :query => "some query")
       get :search, { :page => 10, :query => "some query" }
     end
   end
