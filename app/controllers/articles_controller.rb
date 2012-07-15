8d0046356d4091fc81b354e185b2598a10b79100
Washington Botelho <wbotelhos@gmail.com>
2012-07-15 13:19:09 -0300

    Feature to just update, not publish the article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,7 +40,10 @@ class ArticlesController < ApplicationController
     params[:article][:slug] = article.slug_it(params[:article][:title])
 
     article.update_attributes(params[:article])
-    redirect_to slug_article_path(article.year, article.month, article.day, article.slug), :notice => t("flash.articles.create.notice")
+
+    redirect_to edit_article_path(article), :notice => t("flash.articles.update.notice")
+    #redirect_to slug_article_path(article.year, article.month, article.day, article.slug), :notice => t("flash.articles.create.notice")
+  end
   end
 
   def create
--- spec/integration/article_edit_spec.rb
+++ spec/integration/article_edit_spec.rb
@@ -3,18 +3,20 @@ require "spec_helper"
 
 describe Article, "#create" do
   let!(:user) { FactoryGirl.create(:user) }
+  let!(:article_draft) {
+    FactoryGirl.create(:article, { :created_at => Time.now, :published_at => nil })
+  }
+  let!(:article_published) {
+    FactoryGirl.create(:article, { :created_at => Time.now, :published_at => Time.now })
+  }
 
   before do
     login :with => user.email
   end
 
   context "when article is draft" do
-    let!(:article) {
-      FactoryGirl.create(:article, { :created_at => Time.now, :published_at => nil })
-    }
-
     before do
-      visit edit_article_path(article)
+      visit edit_article_path(article_draft)
     end
 
     it "display draft indicator" do
@@ -23,16 +25,46 @@ describe Article, "#create" do
   end
 
   context "when article is published" do
-    let!(:article) {
-      FactoryGirl.create(:article, { :created_at => Time.now, :published_at => Time.now })
-    }
-
     before do
-      visit edit_article_path(article)
+      visit edit_article_path(article_published)
     end
 
     it "display published indicator" do
       page.should have_selector("div#status", :text => "Publicado")
+    end
+
+    it "hide publish button" do
+      page.should_not have_button("Publicar")
+    end
+  end
+
+  context "when execute update" do
+    before do
+      visit edit_article_path(article_published)
+      click_button "Atualizar"
+    end
+
+    it "redirects to the article page" do
+      current_path.should match(%r[/articles/#{article_published.id}/edit])
+    end
+
+    it "displays success message" do
+      page.should have_content("Artigo atualizado com sucesso!")
+    end
+  end
+
+  context "when execute publish" do
+    before do
+      visit edit_article_path(article_draft)
+      click_button "Publicar"
+    end
+
+    xit "redirects to the article page" do
+      current_path.should eql(slug_article_path(article_draft.year, article_draft.month, article_draft.day, article_draft.slug))
+    end
+
+    xit "displays success message" do
+      page.should have_content("Artigo publicado com sucesso!")
     end
   end
 end
