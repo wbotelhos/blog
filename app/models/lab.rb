fc608264ac78da83ef958f687c1ba00082fae6d4
Washington Botelho <wbotelhos@gmail.com>
2012-09-17 16:45:21 -0300

    Fixed time zone.

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,8 +1,8 @@
 class Lab < ActiveRecord::Base
   attr_accessible :name, :slug, :description
 
-  scope :published, where('published_at is not null and published_at <= ?', Time.now)
-  scope :drafts, where('published_at is null or published_at > ?', Time.now)
+  scope :published, where('published_at is not null and published_at <= ?', Time.zone.now)
+  scope :drafts, where('published_at is null or published_at > ?', Time.zone.now)
 
   validates :name, :slug, :presence => true
   validates :name, :slug, :uniqueness => true
