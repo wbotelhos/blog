62b06a4261c1cc1cf8266cf02f0e4e505152dc91
Washington Botelho <wbotelhos@gmail.com>
2012-09-22 12:58:55 -0300

    Extracted some methods to make it clear.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,17 @@
 class CommentsController < ApplicationController
-
   before_filter :require_login, :only => [:update]
 
   def create
     @article = Article.includes(:user).find(params[:article_id])
 
-    params[:comment][:name] = nil if params[:comment][:name] == I18n.t("activerecord.attributes.comment.name")
-    params[:comment][:email] = nil if params[:comment][:email] == I18n.t("activerecord.attributes.comment.email")
-    params[:comment][:url] = nil if params[:comment][:url] == I18n.t("activerecord.attributes.comment.url")
-    params[:comment][:body] = nil if params[:comment][:body] == %[\n#{I18n.t("activerecord.attributes.comment.body")}] # TODO: why :comment:body preppend one \n?
+    comment = clear_form(params[:comment])
 
-    @comment = @article.comments.new(params[:comment])
+    @comment = @article.comments.new(comment)
     @comment.author = is_logged?
     @comment.comment = Comment.find(params[:comment_id]) unless params[:comment_id].nil?
 
     if @comment.save
-      begin
-        CommentMailer.new(@article, @comment, logger).send
-        flash[:notice] = t("flash.comments.create.notice")
-     rescue Exception => e
-       logger.error "Was not possible to send mail: #{e}"
-       flash[:notice] = t("comment.email_not_sent")
-      end
+      send_mail
     else
       flash[:alert] = t("flash.comments.create.alert")
     end
@@ -43,5 +33,24 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article.year, @article.month, @article.day, @article.slug)
   end
 
+  private
+
+  def clear_form(comment)
+    comment[:name]  = nil if comment[:name]   == I18n.t("activerecord.attributes.comment.name")
+    comment[:email] = nil if comment[:email]  == I18n.t("activerecord.attributes.comment.email")
+    comment[:url]   = nil if comment[:url]    == I18n.t("activerecord.attributes.comment.url")
+    comment[:body]  = nil if comment[:body]   == %[\n#{I18n.t("activerecord.attributes.comment.body")}] # TODO: why :comment:body preppend one \n?
+    comment
+  end
+
+  def send_mail
+     begin
+       CommentMailer.new(@article, @comment, logger).send
+       flash[:notice] = t("flash.comments.create.notice")
+    rescue Exception => e
+      logger.error "Was not possible to send mail: #{e}"
+      flash[:notice] = t("comment.email_not_sent")
+     end
+  end
 end
  
