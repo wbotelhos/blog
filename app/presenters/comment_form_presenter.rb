153a0184f0ad07ce1232c00e4baf811a4861b256
Washington Botelho <wbotelhos@gmail.com>
2012-09-22 12:58:37 -0300

    Minus spaces.

--- app/presenters/comment_form_presenter.rb
+++ app/presenters/comment_form_presenter.rb
@@ -1,12 +1,9 @@
 class CommentFormPresenter
-
   def initialize(article, comment)
-    @article = article
-    @comment = comment
+    @article, @comment = article, comment
   end
 
   def partial
     { :partial => "comments/form", :locals => { :article => @article, :comment => @comment } }
-  end 
-
+  end
 end
