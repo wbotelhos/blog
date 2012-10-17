2100fc9bc9a78589fd414483e1e51cda28546428
Washington Botelho <wbotelhos@gmail.com>
2012-10-16 23:52:43 -0300

    Added remote_cache. Taked off some duplicated data.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,39 @@
-require "bundler/capistrano"
+require 'bundler/capistrano'
 
-set :application, "wbotelhos.com.br"
+set :application, 'wbotelhos.com.br'
 
 set :keep_releases, 3
 
-default_run_options[:pty] = true
-set :ssh_options, { :forward_agent => true }
-set :use_sudo, false
-
 set :scm, :git
-set :repository, "git@github.com:wbotelhos/wbotelhos-com-br.git"
-set :branch, "master"
+set :repository, 'git@github.com:wbotelhos/wbotelhos-com-br.git'
+set :branch, 'master'
+set :deploy_via, :remote_cache
 
-set :user, "ubuntu"
-set :runner, "ubuntu"
-set :group, "ubuntu"
+set :user, 'ubuntu'
+set :runner, 'ubuntu'
+set :group, 'ubuntu'
 set :use_sudo, false
 
-set :deploy_to, "/var/www/wbotelhos-br"
+set :deploy_to, '/var/www/wbotelhos-br'
 set :current, "#{deploy_to}/current"
 
 role :web, application
 role :app, application
-role :db,  application, :primary => true
+role :db,  application, primary: true
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:keys] = "~/.ssh/blogbr.pem"
+ssh_options[:keys] = '~/.ssh/blogbr.pem'
 
-after :deploy, "deploy:cleanup"
-after :deploy, "sphinx:config"
-after :deploy, "sphinx:rebuild"
+after :deploy, 'deploy:cleanup'
+after :deploy, 'sphinx:config'
+after :deploy, 'sphinx:rebuild'
 
 namespace :deploy do
   task :start do
-    %w[
-      config/database.yml
-      config/sphinx.yml
-    ].each do |path|
-      from = "#{deploy_to}/#{path}"
-      to = "#{current}/#{path}"
+    %w[config/database.yml config/sphinx.yml].each do |path|
+      from  = "#{deploy_to}/#{path}"
+      to    = "#{current}/#{path}"
 
       run "if [ -f '#{to}' ]; then rm '#{to}'; fi; ln -s #{from} #{to}"
     end
@@ -58,14 +52,11 @@ namespace :deploy do
 end
 
 namespace :app do
-  desc "Copy configuration files"
+  desc 'Copy configuration files'
   task :setup do
-    %w[
-      config/database.yml
-      config/sphinx.yml
-    ].each do |path|
-      from = "#{deploy_to}/#{path}"
-      to = "#{current}/#{path}"
+    %w[config/database.yml config/sphinx.yml].each do |path|
+      from  = "#{deploy_to}/#{path}"
+      to    = "#{current}/#{path}"
 
       run "if [ -f '#{to}' ]; then rm '#{to}'; fi; ln -s #{from} #{to}"
     end
@@ -73,27 +64,27 @@ namespace :app do
 end
 
 namespace :sphinx do
-  desc "Regenerate Sphinx configuration"
+  desc 'Regenerate Sphinx configuration'
   task :config do
     run "cd #{current} && sudo bundle exec rake ts:config"
   end
 
-  desc "Stopt the Sphinx"
+  desc 'Stopt the Sphinx'
   task :stop do
     run "cd #{current} && sudo bundle exec rake ts:stop"
   end
 
-  desc "Start the Sphinx"
+  desc 'Start the Sphinx'
   task :start do
     run "cd #{current} && sudo bundle exec rake ts:start"
   end
 
-  desc "Rebuild index"
+  desc 'Rebuild index'
   task :rebuild do
     run "cd #{current} && sudo bundle exec rake ts:reindex"
   end
 
-  desc "Index pending delta"
+  desc 'Index pending delta'
   task :index do
     run "cd #{current} && sudo bundle exec rake ts:index"
   end
