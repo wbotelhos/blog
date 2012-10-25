fd2b044e04d8efb8b95d1913d727e4e001f6f5cc
Washington Botelho <wbotelhos@gmail.com>
2012-10-25 04:04:25 -0200

    Test for edit article page.

--- spec/integration/article/article_edit_spec.rb
+++ spec/integration/article/article_edit_spec.rb
@@ -1,74 +1,73 @@
 # coding: utf-8
 require 'spec_helper'
 
-describe Article, "#create" do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:article_draft) { FactoryGirl.create(:article_draft) }
-  let(:article_published) { FactoryGirl.create(:article_published) }
+describe Article, "#edit" do
+  let!(:category) { FactoryGirl.create :category }
+  let!(:article_published) { FactoryGirl.create :article_published, categories: [category] }
 
-  before do
-    login with: user.email
+  context "when logged" do
+    let(:user) { FactoryGirl.create :user }
+
+    before { login with: user.email }
+
+    context "form" do
+      before { visit edit_article_path article_published }
+
+      it { current_path.should match %r(/articles/#{article_published.id}/edit) }
+
+      it { page.should have_field 'article_title', text: article_published.title }
+      it { page.should have_field 'article_body', text: article_published.body }
+      it { page.should have_field "category-#{category.id.to_s}" }
+
+      it "displays all categories" do
+        page.should have_content category.name
+      end
+
+      it "the related category cames checked" do
+        page.should have_checked_field "category-#{category.id.to_s}"
+      end
+    end
+
+    context "while draft" do
+      let!(:article_draft) { FactoryGirl.create :article, published_at: nil, categories: [category] }
+
+      before { visit edit_article_path article_draft }
+
+      it "display draft indicator" do
+        page.should have_selector 'div#status', text: 'Rascunho'
+      end
+
+      it "show preview link" do
+        page.should have_selector 'div#url a', text: 'Visualizar'
+      end
+    end
+
+    context "while published" do
+      before { visit edit_article_path article_published }
+
+      it "displays published indicator" do
+        page.should have_selector 'div#status', text: 'Publicado'
+      end
+
+      it "hide publish button" do
+        page.should_not have_button 'Publicar'
+      end
+
+      it "show slug link" do
+        page.should have_selector 'div#url a', text: article_published.slug
+      end
+    end
   end
 
-  context "when article is draft" do
-    before do
-      visit edit_article_path(article_draft)
+  context "when unlogged" do
+    before { visit edit_article_path article_published.id }
+
+    it "redirects to the login page" do
+      current_path.should == login_path
     end
 
-    it "display draft indicator" do
-      page.should have_selector("div#status", :text => "Rascunho")
-    end
-
-    it "show preview link" do
-      page.should have_selector("div#url a", :text => "Visualizar")
-    end
-  end
-
-  context "when article is published" do
-    before do
-      visit edit_article_path(article_published)
-    end
-
-    it "display published indicator" do
-      page.should have_selector("div#status", :text => "Publicado")
-    end
-
-    it "hide publish button" do
-      page.should_not have_button("Publicar")
-    end
-
-    it "show slug link" do
-      page.should have_selector("div#url a", :text => article_published.slug)
-    end
-  end
-
-  context "when execute update" do
-    before do
-      visit edit_article_path(article_published)
-      click_button "Atualizar"
-    end
-
-    it "redirects to the article page" do
-      current_path.should match(%r[/articles/#{article_published.id}/edit])
-    end
-
-    it "displays success message" do
-      page.should have_content("Artigo atualizado com sucesso!")
-    end
-  end
-
-  context "when execute publish" do
-    before do
-      visit edit_article_path(article_draft)
-      click_button "Publicar"
-    end
-
-    xit "redirects to the article page" do
-      current_path.should eql(article_path(article_draft.year, article_draft.month, article_draft.day, article_draft.slug))
-    end
-
-    xit "displays success message" do
-      page.should have_content("Artigo publicado com sucesso!")
+    it "displays error message" do
+      page.should have_content 'Você precisa estar logado!'
     end
   end
 end
