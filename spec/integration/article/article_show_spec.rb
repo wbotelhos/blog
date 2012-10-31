dc8706da067dea8218e57cc93e4259fe9a1ee0d9
Washington Botelho <wbotelhos@gmail.com>
2012-10-31 14:05:26 -0200

    Article show testing.

--- spec/integration/article/article_show_spec.rb
+++ spec/integration/article/article_show_spec.rb
@@ -2,22 +2,81 @@
 require 'spec_helper'
 
 describe Article, "#show" do
-  let(:article) {
-    FactoryGirl.create(:article_published)
-  }
+  let(:user) { FactoryGirl.create :user, bio: 'this is my bio' }
+  let(:article) { FactoryGirl.create :article_published, user: user }
+  let(:path) { article_path(article.year, article.month, article.day, article.slug) }
 
-  before do
-    visit article_path(article.year, article.month, article.day, article.slug)
-  end
+  before { visit path }
 
   it "redirects to show page" do
-    current_path.should match(%r[/#{article.year}/#{article.month}/#{article.day}/#{article.slug}])
+    current_path.should == path
   end
 
-  context "displaying the resume" do
-    it "remove <!--more--> tag" do
-      page.should_not have_content("<!--more-->")
+  it "display the draft record" do
+    page.should have_content article.title
+    page.should have_content article.text
+  end
+
+  it "show tags with commas" do
+    page.should have_content article.categories.to_a.map(&:name).join ', '
+  end
+
+  it "not display edit link" do
+    page.should_not have_link 'Editar Artigo'
+  end
+
+  it "display permalink" do
+    page.should have_link 'Permalink', href: path
+  end
+
+  it "display comments link" do
+    page.should have_selector 'li.comments a', href: "#{path}#comments"
+  end
+
+  context "author" do
+    it "bio" do
+      page.should have_content article.user.bio
     end
   end
 
+  context "comment numbers" do
+    context "with zero comments" do
+      it "show no one text" do
+        find('li.comments a span').text.should == 'Nenhum comentário, seja o primeiro! (:'
+      end
+    end
+
+    context "with one comment" do
+      before do
+        FactoryGirl.create :comment, article: article
+        visit articles_path
+      end
+
+      it "show the number of comments" do
+        find('li.comments a span').text.should == '1 comentário'
+      end
+
+      context "with two comment" do
+        before do
+          FactoryGirl.create :comment, article: article
+          visit articles_path
+        end
+
+        it "show the number of comments" do
+          find('li.comments a span').text.should == '2 comentários'
+        end
+      end
+    end
+  end
+
+  context "when logged" do
+    before do
+      login with: user.email
+      visit path
+    end
+
+    it "display edit link" do
+      page.should have_link 'Editar Artigo', href: "/articles/#{article.id}/edit"
+    end
+  end
 end
