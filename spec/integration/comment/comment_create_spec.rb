334fc9ac41c1869b379f4aae7dc87a86ea877ce8
Washington Botelho <wbotelhos@gmail.com>
2012-11-08 02:06:22 -0200

    Breaking error message testing.

--- spec/integration/comment/comment_create_spec.rb
+++ spec/integration/comment/comment_create_spec.rb
@@ -2,120 +2,85 @@
 require 'spec_helper'
 
 describe Comment, "#create" do
-  let(:user) { FactoryGirl.create(:user, { id: 1 }) }
-  let(:article) { FactoryGirl.create(:article_published, { id: 1 }) }
+  let(:user) { FactoryGirl.create :user }
+  let(:article) { FactoryGirl.create :article_published }
+  let(:path) { article_path(article.year, article.month, article.day, article.slug) }
 
   context "when logged" do
     before do
       login with: user.email
-      visit article_path(article.year, article.month, article.day, article.slug)
+      visit path
     end
 
     context "with valid data" do
       before do
-        fill_in "comment_body", with: "comment"
-        click_button "Comentar"
+        fill_in 'comment_body', with: 'comment'
+        click_button 'Comentar'
       end
 
       it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
+        current_path.should == path
       end
 
-       it "displays success message" do
-         page.should have_content("Seu comentário foi adicionado!")
-       end
-
-       it "displays comment" do
-         page.should have_content("comment")
-       end
-    end
-
-    context "with invalid data" do
-      before do
-        click_button "Comentar"
+      it "displays success message" do
+        page.should have_content 'Seu comentário foi adicionado!'
       end
 
-      it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
-      end
-
-      it "displays error message" do
-        page.should have_content("Escreva o seu comentário!")
+      it "displays comment" do
+        page.should have_content 'comment'
       end
     end
 
     context "with shadow data" do
-      before do
-        click_button "Comentar"
-      end
+      before { click_button 'Comentar' }
 
       it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
+        current_path.should == path
       end
 
-      xit "displays error message" do
-        page.should have_content("Comentário deve ser preenchido!")
+      it "displays error message" do
+        page.should have_content 'Escreva o seu comentário!'
       end
     end
   end
 
   context "when unlogged" do
-    before do
-      visit article_path(article.year, article.month, article.day, article.slug)
-    end
+    before { visit path }
 
     context "with valid data" do
       before do
-        fill_in "comment_name",   with: "some name"
-        fill_in "comment_email",  with: "some_email@email.com"
-        fill_in "comment_url",    with: "http://some_url.com"
-        fill_in "comment_body",   with: "some comment"
-        click_button "Comentar"
+        fill_in 'comment_name',   with: 'some name'
+        fill_in 'comment_email',  with: 'some_email@email.com'
+        fill_in 'comment_url',    with: 'http://some_url.com'
+        fill_in 'comment_body',   with: 'some comment'
+        click_button 'Comentar'
       end
 
       it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
+        current_path.should == path
       end
 
        it "displays success message" do
-         page.should have_content("Seu comentário foi adicionado!")
+         page.should have_content 'Seu comentário foi adicionado!'
        end
 
        it "displays comment" do
-         page.should have_content("some name")
-         page.should have_content("some comment")
+         page.should have_content 'some name'
+         page.should have_content 'some comment'
        end
     end
 
     context "with invalid data" do
-      before do
-        click_button "Comentar"
-      end
+      before { click_button 'Comentar' }
 
       it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
+        current_path.should == path
       end
 
-      xit "displays error message" do
-        page.should have_content("Nome deve ser preenchido!")
-        page.should have_content("E-mail deve ser preenchido!")
-        page.should have_content("Comentário deve ser preenchido!")
-      end
-    end
-
-    context "with shadow data" do
-      before do
-        click_button "Comentar"
-      end
-
-      it "redirects to the article page" do
-        current_path.should eql(article_path(article.year, article.month, article.day, article.slug))
-      end
-
-      xit "displays error message" do
-        page.should have_content("Nome deve ser preenchido!")
-        page.should have_content("E-mail deve ser preenchido!")
-        page.should have_content("Comentário deve ser preenchido!")
+      it "displays error message" do
+        page.should have_content 'Nome deve ser preenchido!'
+        page.should have_content 'E-mail deve ser preenchido!'
+        page.should have_content 'Comentário deve ser preenchido!'
       end
     end
   end
