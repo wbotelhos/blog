f8ce6d0437145a19c81a11268815fdc9b34072e5
Washington Botelho <wbotelhos@gmail.com>
2012-11-20 17:11:03 -0200

    Just a skeleton to test update comment.

---
+++ spec/integration/comment/comment_show_spec.rb
@@ -0,0 +1,34 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe Comment, "#show" do
+  let(:user) { FactoryGirl.create :user }
+  let(:article) { FactoryGirl.create :article_published, user: user }
+  let(:path) { article_path(article.year, article.month, article.day, article.slug) }
+  let(:comment) { FactoryGirl.create :comment, article: article }
+
+  before { visit path }
+
+  context "when logged" do
+    before do
+      login with: user.email
+      visit path
+    end
+
+    it "edit fields are hidden"
+
+    context "when double click on comment" do
+      it "show fields form"
+    end
+  end
+
+  context "when unlogged" do
+    before { visit path }
+
+    it "edit fields are hidden"
+
+    context "when double click on comment" do
+      it "do not show fields form"
+    end
+  end
+end
