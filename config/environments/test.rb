6c04d4d3b7401a8e3daa3929c13815707f5ec9eb
Washington Botelho <wbotelhos@gmail.com>
2012-11-24 16:06:36 -0200

    Loggin features exceptions.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Blog::Application.configure do
 
   config.action_controller.allow_forgery_protection   = false
   config.action_controller.perform_caching            = false
-  config.action_dispatch.show_exceptions              = false
+  config.action_dispatch.show_exceptions              = true
   config.action_mailer.delivery_method                = :test
   config.active_record.mass_assignment_sanitizer      = :strict
   config.active_support.deprecation                   = :stderr
