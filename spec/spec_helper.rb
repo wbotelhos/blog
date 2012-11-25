dbfe51c6ee8e9fe3bec56ba4ed677e8a29fbb396
Washington Botelho <wbotelhos@gmail.com>
2012-11-25 01:59:36 -0200

    Removed transactional fixture to work with js: true.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,6 @@ require 'rspec/rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
 
   config.before :suite do
