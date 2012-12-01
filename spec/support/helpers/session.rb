1e246b27577e34b965035d4f14e023aaa2ce1082
Washington Botelho <wbotelhos@gmail.com>
2012-11-30 23:13:31 -0200

    Renaming to better names and make condition to redirect to login.

--- spec/support/helpers/session.rb
+++ spec/support/helpers/session.rb
@@ -1,6 +1,6 @@
-module IntegrationHelper
+module SessionHelper
   def login(options)
-    visit login_path
+    visit login_path unless current_path == login_path
 
     fill_in 'E-mail', with: options[:with]
     fill_in 'Senha', with: 'password'
@@ -11,5 +11,5 @@ module IntegrationHelper
 end
 
 RSpec.configure do |config|
-  config.include(IntegrationHelper, type: :request)
+  config.include SessionHelper, type: :feature
 end
