811b9e2b888b4e0e3be52ee95e563353e9e16008
Washington Botelho <wbotelhos@gmail.com>
2012-12-02 00:35:09 -0200

    Config pro player do Database Cleaner to fix mess BD.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,26 @@ require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-DatabaseCleaner.strategy = :truncation, { pre_count: true }
-
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.before :suite do
+    DatabaseCleaner.clean_with :truncation, { pre_count: true }
+  end
+
   config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before js: true do
+    DatabaseCleaner.strategy = :truncation, { pre_count: true }
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
     DatabaseCleaner.clean
   end
 end
