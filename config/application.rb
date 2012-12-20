85daae3b196edf61a9a2211ca54bd38ffcdc2f63
Washington Botelho <wbotelhos@gmail.com>
2012-12-20 01:25:45 -0200

    Taked off field error wrapper.

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,14 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Blog
   class Application < Rails::Application
-    config.time_zone                          = 'Brasilia'
-    config.i18n.load_path                     += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
-    config.i18n.default_locale                = :'pt-BR'
-    config.encoding                           = 'utf-8'
-    config.filter_parameters                  += [:password]
+    config.action_mailer.default_url_options  = { host: 'wbotelhos.com.br' }
+    config.action_view.field_error_proc       = -> tag, _ { tag }
     config.active_record.whitelist_attributes = true
     config.assets.enabled                     = false
-    config.action_mailer.default_url_options  = { host: 'wbotelhos.com.br' }
+    config.encoding                           = 'utf-8'
+    config.filter_parameters                  += [:password]
+    config.i18n.default_locale                = :'pt-BR'
+    config.i18n.load_path                     += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+    config.time_zone                          = 'Brasilia'
   end
 end
