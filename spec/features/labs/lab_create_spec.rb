63db4cb62a038560600a8e8103fe3165003c8b9e
Washington Botelho <wbotelhos@gmail.com>
2012-12-22 18:26:38 -0200

    Validations for uniqueness slug.

--- spec/features/labs/lab_create_spec.rb
+++ spec/features/labs/lab_create_spec.rb
@@ -15,7 +15,7 @@ describe Lab, "#create" do
       fill_in 'lab_slug', with: 'slug'
       fill_in 'lab_description', with: 'description'
       fill_in 'lab_image', with: 'image'
-      click_button 'Criar'
+      click_button 'Salvar'
     end
 
     it "redirects to drafts page" do
@@ -31,7 +31,7 @@ describe Lab, "#create" do
     context "blank name" do
       before do
         fill_in 'lab_name', with: ''
-        click_button 'Criar'
+        click_button 'Salvar'
       end
 
       it "renders form page again" do
@@ -45,7 +45,7 @@ describe Lab, "#create" do
       before do
         fill_in 'lab_name', with: 'name'
         fill_in 'lab_slug', with: ''
-        click_button 'Criar'
+        click_button 'Salvar'
       end
 
       it "renders form page again" do
@@ -53,6 +53,22 @@ describe Lab, "#create" do
       end
 
       it { page.should have_content 'O campo "Slug *" deve ser preenchido!' }
+    end
+
+    context "Name already taken" do
+      let!(:lab) { FactoryGirl.create :lab }
+
+      before do
+        fill_in 'lab_name', with: lab.name
+        fill_in 'lab_slug', with: 'new-slug'
+        click_button 'Salvar'
+      end
+
+      it "renders form page again" do
+        current_path.should == labs_path
+      end
+
+      it { page.should have_content %(O valor "#{lab.name}" para o campo "Nome *" já esta em uso!) }
     end
   end
 end
