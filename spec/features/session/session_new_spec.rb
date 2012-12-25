7c1b0255995b9c915364511e00977879de0d8be8
Washington Botelho <wbotelhos@gmail.com>
2012-12-24 23:46:15 -0200

    js true for anti box test.

--- spec/features/session/session_new_spec.rb
+++ spec/features/session/session_new_spec.rb
@@ -36,7 +36,7 @@ describe User, 'session#new' do
     end
   end
 
-  context "anti bot" do
+  context "anti bot", js: true do
     it 'starts checked' do
       find('#bot').should be_checked
     end
