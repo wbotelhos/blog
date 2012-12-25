23d312c6a68e9ee3eec0e4233754f91c209f8b0e
Washington Botelho <wbotelhos@gmail.com>
2012-12-24 22:07:03 -0200

    Extracted response link.

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -18,11 +18,10 @@ module CommentHelper
     html <<   '<div class="content">'
     html <<     '<div class="name-date">'
     html <<       anchors(comment)
-
-    html <<       %(<span>#{t('comment.created_at', time: time_ago_in_words(comment.created_at))}</span>)
+    html <<       date(comment)
     html <<     '</div>'
 
-    html <<     link("#{request.fullpath}#comment-form", I18n.t('comment.reply'), '_self', 'reply-link')
+    html <<     response_link(comment)
 
     html <<     content_tag(:div, markdown(comment.body), class: 'text')
 
@@ -30,11 +29,11 @@ module CommentHelper
     html <<       hidden_fields
     html <<       fields(comment)
 
-    html <<       pe(link('javascript:void(0);', I18n.t('comment.close'), '', 'close'))
+    html <<       pe(link('javascript:void(0);', t('comment.close'), '', 'close'))
 
     html <<       %(<p class="human"><label for="bot-#{comment.id}">b0t?</label><input id="bot-#{comment.id}" type="checkbox" checked="checked"></p>)
 
-    html <<       pe(input('submit', '', I18n.t('comment.update')))
+    html <<       pe(input('submit', '', t('comment.update')))
     html <<     '</form>'
     html <<   '</div>'
     html << '</div>'
@@ -88,15 +87,15 @@ module CommentHelper
   end
 
   def comment_number(comment)
-    link_to "##{comment.id}", url_anchor(comment), title: I18n.t('comment.shortcut_link')
+    link_to "##{comment.id}", url_anchor(comment), title: t('comment.shortcut_link')
   end
 
   def response_description(comment)
     return '' if comment.comment.nil?
 
-    link = link_to("##{comment.comment.id}", url_anchor(comment.comment), title: I18n.t('comment.go_to_parent'), class: 'parent-anchor')
+    link = link_to("##{comment.comment.id}", url_anchor(comment.comment), title: t('comment.go_to_parent'), class: 'parent-anchor')
 
-    content_tag(:p, I18n.t('comment.reply_to')) + link
+    content_tag(:p, t('comment.reply_to')) + link
   end
 
   def anchors(comment)
@@ -104,6 +103,14 @@ module CommentHelper
       comment_number(comment) +
       link_to(comment.name, comment.url, target: '_blank', class: 'name') +
       response_description(comment), class: 'anchors')
+  end
+
+  def date(comment)
+    content_tag :span , t('comment.created_at', time: time_ago_in_words(comment.created_at))
+  end
+
+  def response_link(comment)
+    link_to t('comment.reply'), "#{request.fullpath}#comment-form", target: '_self', class: 'reply-link'
   end
 
   def link(url, text, target = '', clazz = '')
