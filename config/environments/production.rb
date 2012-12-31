02fef422157840311138e5ef9c40d0b162cb86a3
Washington Botelho <wbotelhos@gmail.com>
2012-12-31 11:50:18 -0200

    Sorting to better find.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,14 @@
 Blog::Application.configure do
-  config.cache_classes                        = true
-  config.consider_all_requests_local          = false
   config.action_controller.perform_caching    = true
-  config.active_support.deprecation           = :notify
-
-  config.action_mailer.raise_delivery_errors  = false
   config.action_mailer.asset_host             = 'http://wbotelhos.com.br'
   config.action_mailer.default_url_options    = { :host => 'wbotelhos.com.br' }
   config.action_mailer.delivery_method        = :smtp
+  config.action_mailer.raise_delivery_errors  = false
+  config.active_support.deprecation           = :notify
+  config.cache_classes                        = true
+  config.consider_all_requests_local          = false
+  config.i18n.fallbacks                       = true
+  config.serve_static_assets                  = false
   config.action_mailer.smtp_settings          = {
     address:                'email-smtp.us-east-1.amazonaws.com',
     user_name:              ENV['SES_USER'],
@@ -16,7 +17,4 @@ Blog::Application.configure do
     enable_starttls_auto:   true,
     port:                   465
   }
-
-  config.serve_static_assets                  = false
-  config.i18n.fallbacks                       = true
 end
