b03e9254bb1308de9b1fab436602c680b95312b4
Washington Botelho <wbotelhos@gmail.com>
2013-01-01 15:17:12 -0200

    Removed unused class.

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -11,20 +11,20 @@ module ArticleHelper
     menus = ''
 
     link = link_to t('article.permalink'), article_slug(article), title: t('article.permalink')
-    menus << menu('link', link, 'permalink')
+    menus << menu('link', link)
 
     link = link_to t('article.comments_count_html', count: article.comments_count), article_slug(article, 'comments'), title: t('comment.other')
-    menus << menu('comments-alt', link, class: 'comments')
+    menus << menu('comments-alt', link)
 
     link = link_to t('article.published_at', time: l(article.published_at)), 'javascript:void(0);', title: t('article.published_date')
-    menus << menu('calendar', link, class: 'published')
+    menus << menu('calendar', link)
 
     link = link_to t('article.share'), article_slug(article, 'share'), title: t('article.share')
-    menus << menu('share', link, class: 'share')
+    menus << menu('share', link)
 
     if is_logged?
-      link_edit = link_to t('navigation.article.edit'), articles_edit_path(article), title: t('article.edit')
-      menus << menu('edit', link_edit, class: 'edit')
+      link = link_to t('navigation.article.edit'), articles_edit_path(article), title: t('article.edit')
+      menus << menu('edit', link)
     end
 
     content_tag :ul, menus.html_safe, class: 'links'
@@ -32,8 +32,8 @@ module ArticleHelper
 
   private
 
-  def menu(icon, link, clazz)
+  def menu(icon, link)
     icon = content_tag :i, nil, class: "icon-#{icon} icon-large"
-    content_tag :li, icon + link, class: clazz
+    content_tag :li, icon + link
   end
 end
