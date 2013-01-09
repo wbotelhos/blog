a6d3c199d9071f6b8670b164f620ae19a930f162
Washington Botelho <wbotelhos@gmail.com>
2013-01-08 23:38:52 -0200

    Using a unique validates declaration.

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -6,8 +6,7 @@ class Lab < ActiveRecord::Base
   scope :published, where('published_at is not null and published_at <= ?', Time.zone.now)
   scope :drafts, where('published_at is null or published_at > ?', Time.zone.now)
 
-  validates :name, :slug, presence: true
-  validates :name, :slug, uniqueness: true
+  validates :name, :slug, presence: true, uniqueness: true
 
   def status
     if created_at.nil?
