14bf0e0bb4a0706bcd4ae47fc1397833d7a33c4d
Washington Botelho <wbotelhos@gmail.com>
2013-01-13 00:22:24 -0200

    Less space on equals.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Blog::Application.configure do
-  config.action_controller.perform_caching                = false
-  config.action_dispatch.best_standards_support           = :builtin
-  config.action_mailer.asset_host                         = 'http://wbotelhos.com.br'
-  config.action_mailer.default_url_options                = { host: 'wbotelhos.com.br' }
-  config.action_mailer.delivery_method                    = :smtp
-  config.action_mailer.raise_delivery_errors              = true
-  config.action_mailer.smtp_settings                      = { address: 'localhost', port: 1025 }
-  config.active_record.auto_explain_threshold_in_seconds  = 0.5
-  config.active_record.mass_assignment_sanitizer          = :strict
-  config.active_support.deprecation                       = :log
-  config.cache_classes                                    = false
-  config.consider_all_requests_local                      = true
-  config.whiny_nils                                       = true
+  config.action_controller.perform_caching               = false
+  config.action_dispatch.best_standards_support          = :builtin
+  config.action_mailer.asset_host                        = 'http://wbotelhos.com.br'
+  config.action_mailer.default_url_options               = { host: 'wbotelhos.com.br' }
+  config.action_mailer.delivery_method                   = :smtp
+  config.action_mailer.raise_delivery_errors             = true
+  config.action_mailer.smtp_settings                     = { address: 'localhost', port: 1025 }
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.mass_assignment_sanitizer         = :strict
+  config.active_support.deprecation                      = :log
+  config.cache_classes                                   = false
+  config.consider_all_requests_local                     = true
+  config.whiny_nils                                      = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,15 @@
 Blog::Application.configure do
-  config.action_controller.perform_caching    = true
-  config.action_mailer.asset_host             = 'http://wbotelhos.com.br'
-  config.action_mailer.default_url_options    = { host: 'wbotelhos.com.br' }
-  config.action_mailer.delivery_method        = :smtp
-  config.action_mailer.raise_delivery_errors  = false
-  config.active_support.deprecation           = :notify
-  config.cache_classes                        = true
-  config.consider_all_requests_local          = false
-  config.i18n.fallbacks                       = true
-  config.serve_static_assets                  = false
-  config.action_mailer.smtp_settings          = {
+  config.action_controller.perform_caching   = true
+  config.action_mailer.asset_host            = 'http://wbotelhos.com.br'
+  config.action_mailer.default_url_options   = { host: 'wbotelhos.com.br' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.active_support.deprecation          = :notify
+  config.cache_classes                       = true
+  config.consider_all_requests_local         = false
+  config.i18n.fallbacks                      = true
+  config.serve_static_assets                 = false
+  config.action_mailer.smtp_settings         = {
     address:                'email-smtp.us-east-1.amazonaws.com',
     user_name:              ENV['SES_USER'],
     password:               ENV['SES_PASSWORD'],
