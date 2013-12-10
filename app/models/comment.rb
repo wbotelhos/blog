52a280aebf822d26e2bcd6adca995cf5273b7ef1
Washington Botelho <wbotelhos@gmail.com>
2013-12-10 18:25:52 -0200

    remove: unused read only. counter_cache. relation now made by act_as_tree.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,16 +2,13 @@ class Comment < ActiveRecord::Base
   include ActsAsTree
   extend ActsAsTree::Presentation
 
-  attr_readonly :user_id
   attr_accessible :name, :email, :url, :body, :article_id, :parent_id
 
-  belongs_to :article, counter_cache: true
-  belongs_to :comment
-  # has_many :comments
+  belongs_to :article
 
-  scope :roots, -> { where parent_id: nil }#
+  scope :roots, -> { where parent_id: nil }
 
-  acts_as_tree order: 'id desc'#
+  acts_as_tree order: 'id desc'
 
   validates :name, :body, :article, presence: true
   validates :email,                 presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
