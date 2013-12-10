4f05caa3f33c3761087733f33e8d5f5cda52aa30
Washington Botelho <wbotelhos@gmail.com>
2013-12-10 16:09:23 -0200

    remove: category test models.

--- spec/models/category/assignment_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Category, 'assignment' do
-  subject { Category.new name: 'name' }
-
-  its(:name) { should == 'name' }
-end
--- spec/models/category/category_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Category do
-  it 'has a valid factory' do
-    FactoryGirl.build(:category).should be_valid
-  end
-end
