584f9d529bb70fc1b5ce5eaacd78e39234ff784a
Washington Botelho <wbotelhos@gmail.com>
2013-12-11 02:20:01 -0200

    fix: user model specs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :bio, :url, :github, :linkedin, :twitter, :facebook, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 
   attr_reader :password
 
-  validates :email,     presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :name,      presence: true
-  validates :password,  presence: true, confirmation: true
+  validates :email    , presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :name     , presence: true
+  validates :password , presence: true, confirmation: true
 
   has_many :articles, dependent: :nullify
 
--- spec/models/user/user_spec.rb
+++ spec/models/user/user_spec.rb
@@ -4,4 +4,51 @@ describe User do
   it 'has a valid factory' do
     FactoryGirl.build(:user).should be_valid
   end
+
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :password }
+
+  xit { should validate_uniqueness_of :email }
+
+  context :create do
+    it 'creates a valid media' do
+      User.new(
+        email:                 'john@example.org',
+        name:                  'John',
+        password:              'password',
+        password_confirmation: 'password'
+      ).should be_valid
+    end
+  end
+
+  context :format do
+    it 'validates the email format' do
+      expect(FactoryGirl.build(:user, email: 'fail')).to be_invalid
+      expect(FactoryGirl.build(:user, email: 'fail@')).to be_invalid
+      expect(FactoryGirl.build(:user, email: 'fail@fail')).to be_invalid
+      expect(FactoryGirl.build(:user, email: 'fail@fail.')).to be_invalid
+
+      expect(FactoryGirl.build(:user, email: 'ok@ok.ok')).to be_valid
+    end
+  end
+
+  context :uniqueness do
+    let(:user) { FactoryGirl.create :user }
+
+    it 'does not allow the same email'  do
+      expect(FactoryGirl.build(:user, email: user.email)).to be_invalid
+    end
+  end
+
+  context :confirmation do
+    it 'has a invalid one' do
+      User.new(
+        email:                 'john@example.org',
+        name:                  'John',
+        password:              'password',
+        password_confirmation: 'password_wrong'
+      ).should_not be_valid
+    end
+  end
 end
