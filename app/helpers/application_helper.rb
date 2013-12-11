8b3d3a6f7022ddda2a1d173071609ecdd0ad40c9
Washington Botelho <wbotelhos@gmail.com>
2013-12-11 17:43:40 -0200

    fix: application helper spec.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,21 @@
-# coding: utf-8
-
 module ApplicationHelper
-  # workaround to simulate the form_builder that does not works.
-  def errors_for(object, key)#
+  def donate_button(item_name = 'wbotelhos.com') # pending
+    url   = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=X8HEP2878NDEG&item_name=#{item_name}"
+    label = content_tag(:i, nil, class: 'i-heart') + t(:donate)
+
+    link_to label, url, id: :donate, target: :_blank
+  end
+
+  def errors_for(object, key)
     errors = object.errors.messages[key]
 
     content_tag(:span, errors[0], class: 'validation-error') if errors.present?
   end
 
-  def menu_icon(text, clazz, path) #
-    link_to '', path, alt: text, title: text, class: clazz
-  end
-
-  def social_icon(text, clazz, path) #
-    text = "#{text} <#{path.gsub /mailto:/, ''}>"
-
-    link_to '', path, alt: text, title: text, target: :_blank, class: clazz
-  end
-
-  def author(user)
-    ''.html_safe.tap do |html|
-      html << gravatar(user.email, alt: '', title: user.name)
-      html << (content_tag :div, simple_format(user.bio.html_safe), class: 'biography' if user.bio)
-      html << (content_tag :div, author_social(user).html_safe, class: 'social')
-    end
-  end
-
   def gravatar(email, options = {})
     hash = Digest::MD5.hexdigest(email)
+    url  = "http://www.gravatar.com/avatar/#{hash}?d=mm"
 
-    url = "http://www.gravatar.com/avatar/#{hash}?d=mm"
     url << "&s=#{options[:size]}" if options[:size]
 
     options[:alt] = '' if options[:alt].nil?
@@ -37,21 +23,14 @@ module ApplicationHelper
     image_tag url, options
   end
 
-  def logo
-    h1 = content_tag :h1, link_to(CONFIG['author'], root_path)
-    pe = content_tag :p, CONFIG['description']
-
-    content_tag :div, h1 + pe, id: 'logo'
-  end
-
   def markdown(content)
-    renderer = HTMLwithPygments.new hard_wrap: true # filter_html: true
+    renderer = HTMLwithPygments.new hard_wrap: true
 
     options = {
       autolink:           true,
-      no_intra_emphasis:  true,
       fenced_code_blocks: true,
       lax_html_blocks:    true,
+      no_intra_emphasis:  true,
       strikethrough:      true,
       superscript:        true
     }
@@ -59,14 +38,17 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 
-  def donate_button(item_name = 'wbotelhos.com')
-    url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=X8HEP2878NDEG&item_name=#{item_name}"
-    label = content_tag(:i, nil, class: 'icon-heart-empty') + t('navigation.donate')
-
-    link_to label, url, id: 'donate', target: '_blank'
+  def menu_icon(text, clazz, path)
+    link_to '', path, alt: text, title: text, class: clazz
   end
 
-  def time_ago(time)#
+  def social_icon(text, clazz, path)
+    text = "#{text} <#{path.gsub /mailto:/, ''}>"
+
+    link_to '', path, alt: text, title: text, target: :_blank, class: clazz
+  end
+
+  def time_ago(time)
     value = t('comment.created_at', time: time_ago_in_words(time))
 
     content_tag :abbr, value, title: time.getutc.iso8601 if time
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,113 +1,44 @@
-# coding: utf-8
-
 require 'spec_helper'
 require 'digest/md5'
 
 describe ApplicationHelper do
   describe '#gravatar' do
     let(:email) { 'wbotelhos@gmail.com' }
-    let(:md5) { Digest::MD5.hexdigest email }
+    let(:md5)   { Digest::MD5.hexdigest email }
 
     context 'without :size' do
-      subject { helper.gravatar email }
-
-      it 'build a image' do
-        subject.should match %r(src="http://www\.gravatar\.com/avatar/#{md5}\?d=mm")
+      it 'build an image' do
+        expect(helper.gravatar email).to match %r(src="http://www\.gravatar\.com/avatar/#{md5}\?d=mm")
       end
     end
 
     context 'without alt parameter' do
-      subject { helper.gravatar email }
-
       it 'builds the html with empty alt' do
-        subject.should match 'alt=""'
+        expect(helper.gravatar email).to match 'alt=""'
       end
     end
 
     context 'with alt parameter' do
-      subject { helper.gravatar email, alt: 'alt' }
-
       it 'builds the html with alt' do
-        subject.should match 'alt="alt"'
-      end
-    end
-  end
-
-  describe '#author' do
-    let(:user) { FactoryGirl.build :user, bio: 'bio.com', github: 'github.com', linkedin: 'linkedin.com', twitter: 'twitter.com', facebook: 'facebook.com' }
-    let(:md5) { Digest::MD5.hexdigest user.email }
-
-    subject { helper.author user }
-
-    it 'builds the html' do
-      subject.should match 'alt=""'
-      subject.should match %r(src="http://www\.gravatar\.com/avatar/#{md5}\?d=mm")
-      subject.should match '<div class="biography"><p>bio.com</p></div>'
-      subject.should match '<div class="social">'
-      subject.should match 'href="github.com"'
-      subject.should match 'href="linkedin.com"'
-      subject.should match 'href="twitter.com"'
-      subject.should match 'href="facebook.com"'
-      subject.should match 'class="github"'
-      subject.should match 'class="linkedin"'
-      subject.should match 'class="twitter"'
-      subject.should match 'class="facebook"'
-    end
-  end
-
-  describe '#logo' do
-    it 'builds the html' do
-      helper.logo.should == '<div id="logo"><h1><a href="/">Washington Botelho</a></h1><p>Se você não tem dom, tenha vontade!</p></div>'
-    end
-  end
-
-  describe '#markdown' do
-    it 'works' do
-      helper.markdown('**strong within p**').should == "<p><strong>strong within p</strong></p>\n"
-    end
-  end
-
-  describe '#title' do
-    context 'without section and content_for' do
-      it 'returns just the author name' do
-        helper.title.should == 'Washington Botelho'
-      end
-    end
-
-    context 'with manual section' do
-      it 'returns the author name with the manual name' do
-        helper.title('manual-title').should == 'Washington Botelho | manual-title'
-      end
-    end
-
-    context 'with content_for' do
-      before { view.stub(:content_for).with(:title).and_return 'content-title' }
-
-      xit 'returns it dynamic value' do
-        helper.title.should == 'Washington Botelho | content-title'
+        expect(helper.gravatar(email, alt: :alt)).to match 'alt="alt"'
       end
     end
   end
 
   describe '#donate_button' do
-    let!(:item_name) { 'wbotelhos\.com' }
-    let!(:href) { %r(href="https://www\.paypal\.com/cgi-bin/webscr\?cmd=_donations&amp;business=X8HEP2878NDEG&amp;item_name=#{item_name}") }
+    let(:item_name) { 'wbotelhos\.com' }
 
     context 'without item_name parameter' do
-      it 'buils the html' do
-        helper.donate_button.should match %r(href="https://www\.paypal.com/cgi-bin/webscr\?cmd=_donations&amp;business=X8HEP2878NDEG&amp;item_name=#{item_name})
-        helper.donate_button.should match 'id="donate"'
-        helper.donate_button.should match '<i class="icon-heart-empty"></i>Donate'
+      it 'builds the html' do
+        expect(helper.donate_button).to match %r(https://www\.paypal.com/cgi-bin/webscr\?cmd=_donations&amp;business=X8HEP2878NDEG&amp;item_name=#{item_name})
       end
     end
 
     context 'with item_name parameter' do
-      let!(:item_name) { 'some_item_name' }
+      let(:item_name) { 'some_item_name' }
 
-      it 'buils the html' do
-        helper.donate_button(item_name).should match %r(href="https://www\.paypal.com/cgi-bin/webscr\?cmd=_donations&amp;business=X8HEP2878NDEG&amp;item_name=#{item_name})
-        helper.donate_button(item_name).should match 'id="donate"'
-        helper.donate_button(item_name).should match '<i class="icon-heart-empty"></i>Donate'
+      it 'builds the html' do
+        expect(helper.donate_button item_name).to match %r(https://www\.paypal.com/cgi-bin/webscr\?cmd=_donations&amp;business=X8HEP2878NDEG&amp;item_name=#{item_name})
       end
     end
   end
