389424c7257376030ad874361682cb3637e18ece
Washington Botelho <wbotelhos@gmail.com>
2013-12-11 17:43:47 -0200

    remove: block.

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,7 @@ Blog::Application.routes.draw do
 
   resources :labs, except: [:destroy, :show]
 
-  resources :users, only: :update do
-  end
+  resources :users, only: :update
 
   get '/:slug' , to: 'labs#show'     , as: :slug, constraints: ParamConstraints.new
   get '/:slug' , to: 'articles#show' , as: :slug
