365f346a13b3d1fa589c2e808300230d0b853c79
Washington Botelho <wbotelhos@gmail.com>
2013-12-12 00:47:48 -0200

    fix: articles helper spec.

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,9 +1,9 @@
 module ArticleHelper
-  def article_slug(article, anchor = nil) #
+  def article_slug(article, anchor = nil)
     slug_path(article.slug, anchor: anchor)
   end
 
-  def article_slug_url(article, anchor = nil) #
+  def article_slug_url(article, anchor = nil)
     slug_url(article.slug, anchor: anchor)
   end
 
@@ -11,7 +11,7 @@ module ArticleHelper
     l article.published_at || article.created_at
   end
 
-  def twitter_button(options = {})#
+  def twitter_button(options = {})
     options = {
       text: %("#{options[:text]}" ~ ),
       url:  options[:url],
@@ -23,39 +23,9 @@ module ArticleHelper
     content_tag :div, link, class: :twitter
   end
 
-  def article_menu(article)
-    html = ''
-    html << menu('link',         link_to(t('article.permalink'),       article_slug(article),             title: t('article.permalink')))
-    html << menu('comments-alt', link_to(comments_label(article),      article_slug(article, 'comments'), title: t('comment.other')))
-    html << menu('calendar',     link_to(published_label(article),     'javascript:void(0);',             title: t('article.published_date')))
-    html << menu('share',        link_to(t('article.share'),           article_slug(article, 'share'),    title: t('article.share')))
-    html << menu('edit',         link_to(t('navigation.article.edit'), articles_edit_path(article),       title: t('article.edit'))) if is_logged?
-
-    content_tag :ul, html.html_safe, class: 'links'
-  end
-
-  def tags(article)
-    article.categories.map do |category|
-      link_to category.name, categories_show_path(category.name.slug), title: category.name
-    end.join(' ').html_safe
-  end
-
   private
 
-  def to_query(options)#
+  def to_query(options)
     options.map { |key, value| "#{key}=#{CGI.escape value}" }.join '&'
-  end
-
-  def comments_label(article)
-    t 'comment.count_html', count: article.comments_count
-  end
-
-  def published_label(article)
-    t 'article.published_at', time: l(article.published_at)
-  end
-
-  def menu(icon, link)
-    icon = content_tag :i, nil, class: "icon-#{icon} icon-large"
-    content_tag :li, icon + link
   end
 end
--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -1,53 +1,77 @@
 require 'spec_helper'
 
 describe ArticleHelper do
-  let(:category_1) { FactoryGirl.create :category, name: 'Banana' }
-  let(:category_2) { FactoryGirl.create :category, name: 'Abacaxi' }
-  let!(:article) {
-    FactoryGirl.build :article_published,
-      slug:         'some-slug',
-      published_at: Time.local(1984, 10, 23),
-      category_ids: [category_2.id, category_1.id]
-  }
-  let(:date) { article.published_at }
-
   describe '#article_slug' do
+    let(:article) { FactoryGirl.create :article, title: 'Some Title' }
+
     context 'without anchor' do
       it 'builds the path without anchor' do
-        helper.article_slug(article).should == "/#{date.year}/#{date.month}/#{date.day}/some-slug"
+        expect(helper.article_slug article).to eq '/some-title'
       end
     end
 
     context 'with anchor' do
       it 'builds the path with anchor' do
-        helper.article_slug(article, 'anchor').should == "/#{date.year}/#{date.month}/#{date.day}/some-slug#anchor"
+        expect(helper.article_slug article, 'anchor').to eq '/some-title#anchor'
       end
     end
   end
 
   describe '#article_slug_url' do
-    before { @request.host = 'url.com' }
+    let(:article) { FactoryGirl.create :article, title: 'Some Title' }
+
+    before { @request.host = 'example.org' }
 
     context 'without anchor' do
       it 'builds the path without anchor' do
-        helper.article_slug_url(article).should == "http://url.com/#{date.year}/#{date.month}/#{date.day}/some-slug"
+        expect(helper.article_slug_url article).to eq 'http://example.org/some-title'
       end
     end
 
     context 'with anchor' do
       it 'builds the path with anchor' do
-        helper.article_slug_url(article, 'anchor').should == "http://url.com/#{date.year}/#{date.month}/#{date.day}/some-slug#anchor"
+        expect(helper.article_slug_url article, 'anchor').to eq 'http://example.org/some-title#anchor'
       end
     end
   end
 
-  describe '#tags' do
-    let(:link_1) { '<a href="/categories/abacaxi" title="Abacaxi">Abacaxi</a>' }
-    let(:link_2) { '<a href="/categories/banana" title="Banana">Banana</a>' }
+  describe '#published_at' do
+    context 'with published_at date' do
+      let(:article) { FactoryGirl.build :article, published_at: Time.local(2000, 1, 1) }
 
-    it 'builds the tags as link separated by comma' do
-      helper.tags(article).should == "#{link_1} #{link_2}"
+      it 'returns the published date formated' do
+        expect(helper.published_at article).to eq '01 de Janeiro de 2000'
+      end
+    end
+
+    context 'without published_at date' do
+      let(:article) { FactoryGirl.build :article, created_at: Time.local(2001, 1, 1) }
+
+      it 'returns the created date formated' do
+        expect(helper.published_at article).to eq '01 de Janeiro de 2001'
+      end
     end
   end
 
+  describe '#twitter_button' do
+    context '' do
+      let(:button) { CGI.unescape helper.twitter_button(text: 'Some Text', url: 'http://wbotelhos.com') }
+
+      it 'builds the right text' do
+        expect(button).to match %r(text="Some Text" ~ )
+      end
+
+      it 'builds the right url' do
+        expect(button).to match %r(url=http://wbotelhos.com)
+      end
+
+      it 'builds the right via' do
+        expect(button).to match %r(via=wbotelhos)
+      end
+
+      it 'builds the right target' do
+        expect(button).to match %r(target="_blank")
+      end
+    end
+  end
 end
