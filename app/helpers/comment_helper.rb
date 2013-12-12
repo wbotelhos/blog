d3625359880b8262ea14fdf35c20f5ee27005883
Washington Botelho <wbotelhos@gmail.com>
2013-12-12 01:07:35 -0200

    fix: comment helper spec.

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,11 +1,9 @@
-# coding: utf-8
-
 module CommentHelper
-  def anchor(comment)#
+  def anchor(comment)
     "##{id_for comment}"
   end
 
-  def comment_name(comment, options = {})#
+  def comment_name(comment, options = {})
     if comment.url.present?
       url = comment.url
 
@@ -17,53 +15,11 @@ module CommentHelper
     link_to comment.name, url, options
   end
 
-  def id_for(comment)#
+  def id_for(comment)
     "comment-#{comment.id}"
   end
 
-  def self_anchor(comment)#
+  def self_anchor(comment)
     link_to "##{comment.id}", anchor(comment), title: t('comment.shortcut'), class: :anchor
-  end
-
-  private
-
-  def response_description(comment)
-    return '' if comment.comment.nil?
-
-    link = link_to("##{comment.comment.id}", url_anchor(comment.comment), title: t('comment.go_to_parent'), class: 'parent-anchor')
-
-    content_tag(:p, t('comment.reply_to')) + link
-  end
-
-  def fields(comment)
-    content_tag(:p,
-      input('text',   'comment[name]',  comment.name) +
-      input('text',   'comment[email]', comment.email) +
-      input('text',   'comment[url]',   comment.url) +
-      input('hidden', 'bot',            'yes')
-    ) +
-
-    content_tag(:p,
-      content_tag(:textarea, comment.body, name: 'comment[body]', rows: 20, cols: 30)
-    )
-  end
-
-  def form_closer(comment)
-    content_tag :p, link_to(t('comment.close'), 'javascript:void(0);', class: 'close')
-  end
-
-  def submit_button
-    content_tag :p, submit_tag(t('comment.update'), name: nil)
-  end
-
-  def form(article, comment)
-    anchor    = anchor(comment)
-    onsubmit  = "return l00s3r('bot-#{comment.id}');"
-    style     = 'display: none;'
-    url       = comments_update_path(article, comment, anchor: anchor)
-
-    form_tag(url, method: :put, onsubmit: onsubmit, style: style) do
-      fields(comment) << form_closer(comment) << anti_bot(comment) << submit_button
-    end
   end
 end
