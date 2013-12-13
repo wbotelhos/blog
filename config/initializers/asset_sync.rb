afc7f4288412040f210f4b42110f6c304c3dd49a
Washington Botelho <wbotelhos@gmail.com>
2013-12-12 23:40:18 -0200

    add: asset sync config.

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,9 @@
+AssetSync.configure do |config|
+  config.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  config.fog_directory         = 'estacione'
+  config.fog_provider          = 'AWS'
+  config.fog_region            = 'sa-east-1'
+  config.gzip_compression      = true
+  config.manifest              = true
+end unless Rails.env.test?
