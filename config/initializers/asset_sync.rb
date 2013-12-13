2520e9db699878239b5a1d3f7af56a84890457e5
Washington Botelho <wbotelhos@gmail.com>
2013-12-13 00:13:43 -0200

    fix: bucket name.

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,7 +1,7 @@
 AssetSync.configure do |config|
   config.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.fog_directory         = 'estacione'
+  config.fog_directory         = 'wbotelhos'
   config.fog_provider          = 'AWS'
   config.fog_region            = 'sa-east-1'
   config.gzip_compression      = true
