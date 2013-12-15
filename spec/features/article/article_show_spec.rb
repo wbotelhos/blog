076945f7ab2c8bc2a362d7715bc017344741cfc6
Washington Botelho <wbotelhos@gmail.com>
2013-12-15 20:52:07 -0200

    fix: article show spec.

--- spec/features/article/article_show_spec.rb
+++ spec/features/article/article_show_spec.rb
@@ -1,90 +1,68 @@
 # coding: utf-8
+
 require 'spec_helper'
 
 describe Article, '#show' do
-  let(:user) { FactoryGirl.create :user, bio: 'this is my bio' }
-  let(:article) { FactoryGirl.create :article_published, user: user }
-  let(:path) { article_path(article.year, article.month, article.day, article.slug) }
+  let(:article) { FactoryGirl.create :article_published }
 
-  before { visit path }
+  before { visit slug_path article.slug }
+
+  it 'shows the title' do
+    within 'header' do
+      expect(page).to have_link article.title
+    end
+  end
+
+  it 'shows published date' do
+    within 'header' do
+      expect(page).to have_content "#{article.published_at.day} de Outubro de #{article.published_at.year}"
+    end
+  end
+
+  it 'shows twitter button', :focus do
+    expect(page).to have_link 'Tweet'
+  end
+
+  context 'clicking on title' do
+    before { click_link article.title }
+
+    it 'sends to the same page' do
+      expect(current_path).to eq "/#{article.slug}"
+    end
+  end
 
   it 'redirects to show page' do
-    current_path.should == path
+    expect(current_path).to eq "/#{article.slug}"
   end
 
-  it 'display the draft record' do
-    page.should have_content article.title
-    page.should have_content article.text
-  end
-
-  it 'show tags with commas' do
-    page.should have_content article.categories.to_a.map(&:name).join ', '
-  end
-
-  it 'not display edit link' do
-    page.should_not have_link 'Editar', href: "/articles/#{article.id}/edit"
-  end
-
-  it 'display permalink' do
-    page.should have_link 'Permalink', href: path
-  end
-
-  it 'display comments link' do
-    page.should have_link 'Nenhum comentário até o momento', href: "#{path}#comments"
-  end
-
-  context 'author' do
-    it 'bio' do
-      page.should have_content article.user.bio
+  it 'does not display edit link' do
+    within 'header' do
+      expect(page).to_not have_link 'Editar'
     end
   end
 
-  context 'comment numbers' do
-    context 'with zero comments' do
-      it 'show no one text' do
-        page.should have_content 'Nenhum comentário até o momento'
-      end
+  context 'trying to access an inexistent record' do
+    before { visit slug_path 'invalid' }
+
+    it 'redirects to root page' do
+      expect(current_path).to eq root_path
     end
-
-    context 'with one comment' do
-      before do
-        FactoryGirl.create :comment, article: article
-        visit articles_path
-      end
-
-      it 'show the number of comments' do
-        page.should have_content '1 comentário'
-      end
-
-      context 'with two comment' do
-        before do
-          FactoryGirl.create :comment, article: article
-          visit articles_path
-        end
-
-        it 'show the number of comments' do
-          page.should have_content '2 comentários'
-        end
-      end
-    end
-  end
-
-  context "trying to access a inexistent record" do
-    before { visit article_path 2000, '01', '01', 'slug' }
 
     it 'display not found message' do
-      find('.alert').should have_content 'O artigo "/2000/01/01/slug" não foi encontrado!'
+      expect(page).to have_content 'O artigo "invalid" não foi encontrado!'
     end
   end
 
   context 'when logged' do
     before do
-      login with: user.email
-      visit path
+      login
+      visit slug_path article.slug
     end
 
-    it 'not display edit link' do
-      page.should have_link 'Editar', href: "/articles/#{article.id}/edit"
+    it 'does not display edit link' do
+      within 'header' do
+        expect(page).to have_link 'Editar'
+      end
     end
   end
 end
