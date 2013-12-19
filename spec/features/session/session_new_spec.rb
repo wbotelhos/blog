081faa5dc6a384c540d5a03e4117bce41a753227
Washington Botelho <wbotelhos@gmail.com>
2013-12-19 00:35:23 -0200

    add: session new spec.

--- spec/features/session/session_new_spec.rb
+++ spec/features/session/session_new_spec.rb
@@ -1,71 +1,74 @@
 # coding: utf-8
+
 require 'spec_helper'
 
-describe User, 'session#new' do
+describe User, 'session#new', :js do
   let(:user) { FactoryGirl.create :user }
 
   before { visit login_path }
 
   context 'with wrong password' do
     before do
-      fill_in 'email', with: 'invalid@mail.com'
-      fill_in 'password', with: 'wrong-password'
-      uncheck 'bot'
-      click_button 'Acessar'
+      fill_in 'email'    , with: 'john@example.org'
+      fill_in 'password' , with: 'wrong'
+
+      uncheck 'not_human'
+
+      click_button 'ACESSAR'
     end
 
     it 'redirects to the same page' do
-      current_path.should == login_path
+      expect(current_path).to eq login_path
     end
 
     it 'displays error message' do
-      within '#container-login' do
-        page.should have_content 'E-mail ou senha inválida!'
-      end
+      expect(page).to have_content 'E-mail ou senha inválida!'
     end
   end
 
   context 'with right password' do
     before do
-      fill_in 'email', with: user.email
-      fill_in 'password', with: user.password
-      uncheck 'bot'
-      click_button 'Acessar'
+      fill_in 'email'    , with: user.email
+      fill_in 'password' , with: user.password
+
+      uncheck 'not_human'
+
+      click_button 'ACESSAR'
     end
 
     it 'redirects to admin page' do
-      current_path.should == admin_path
+      expect(current_path).to eq admin_path
     end
   end
 
-  context 'anti bot', js: true do
+  describe '#AntiBOT' do
     it 'starts checked' do
-      find('#bot').should be_checked
+      expect(page).to have_checked_field 'not_human'
     end
 
     it 'starts with bot log' do
-      find('#human label').should have_content 'b0t?'
+      expect(page).to have_content 'BOT!'
     end
 
     context 'on uncheck' do
-      before { uncheck 'bot' }
+      before { uncheck 'not_human' }
 
-      it 'log human message' do
-        find('#human label').should have_content 'human! <3'
+      it 'logs human message' do
+        expect(page).to have_content 'Humanos! <3'
       end
 
       context 'on check' do
-        before { check 'bot' }
+        before { check 'not_human' }
 
         it 'log human message' do
-          find('#human label').should have_content 'stupid! :/'
+          expect(page).to have_content 'Sério?'
         end
 
         context 'and submit' do
-          before { click_button 'Acessar' }
+          before { click_button 'ACESSAR' }
 
-          it 'blocks and log looser message' do
-            find('#human label').should have_content 'b0t? l00s3r!'
+          it 'blocks and shows exclamation message' do
+            expect(page).to have_content 'Hey! Me desmarque.'
           end
         end
       end
@@ -73,14 +76,13 @@ describe User, 'session#new' do
   end
 
   context 'when logged' do
-    before do
-      login with: user.email
-      visit login_path
-    end
+    before { login }
 
     context 'and visit login page' do
+      before { visit login_path }
+
       it 'redirects to index page' do
-        current_path.should == root_path
+        expect(current_path).to eq root_path
       end
     end
   end
