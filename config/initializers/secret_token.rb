fb811c65c9ec3dffbe1b4e65f6bddeaf1be056de
Washington Botelho <wbotelhos@gmail.com>
2013-12-23 00:01:00 -0200

    extract: secret_key to env.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,1 @@
-Blog::Application.config.secret_token = 'c16acb9fb060c041aca95b78fd519654ad4352a1cccc465a223c95a3a67e3832d96a099dc8a0402ca240b21233f8216d489a7cfa7ac2b834bcacf6eeb46b2d95'
+Blog::Application.config.secret_token = ENV['SECRET_TOKEN']
