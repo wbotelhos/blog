84cfa49536d783c9b91bb44b6b683044e72e022e
Washington Botelho <wbotelhos@gmail.com>
2013-12-28 03:11:12 -0200

    extract: find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
   before_filter :require_login
+  before_filter :find, only: [:edit, :update]
 
   def edit
-    @user = User.find session[:user_id]
   end
 
   def update
-    @user = User.find session[:user_id]
-
     filter_unchanged_password
 
     if @user.update_attributes params[:user]
@@ -21,5 +19,9 @@ class UsersController < ApplicationController
 
   def filter_unchanged_password
     params[:user].delete(:password) if params[:user][:password].blank?
+  end
+
+  def find
+    @user = User.find session[:user_id]
   end
 end
