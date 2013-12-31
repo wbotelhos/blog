3ccab60f5b1a033e0719f33fc024573ccdc7b5ee
Washington Botelho <wbotelhos@gmail.com>
2013-12-30 22:30:55 -0200

    remove: redundant check.

--- lib/custom_logger.rb
+++ lib/custom_logger.rb
@@ -12,7 +12,7 @@ class CustomLogger < Rails::Rack::Logger
     request = ActionDispatch::Request.new env
     params  = request.request_parameters
 
-    if params.include?(:bot) && params[:bot].present?
+    if params[:bot].present?
       logger.warn %(B0T on "#{env['PATH_INFO']}"!)
 
       Rails.logger.level = Logger::ERROR
