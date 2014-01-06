1e5bc3137d95502e33c85bd00dab277aaa3ca479
Washington Botelho <wbotelhos@gmail.com>
2014-01-06 16:59:42 -0200

    update: cloning the repo when it does not exist.

--- script/labs/pull.sh
+++ script/labs/pull.sh
@@ -4,9 +4,16 @@
 . script/labs/config.sh
 
 for LAB in "${LABS[@]}"; do
-  cd ~/workspace/"${LAB}"
+  cd ~/workspace
 
-  echo -e "\n${GRAY}Pulling ${LAB}...${NO_COLOR}"
+  if [ -d "${LAB}" ]; then
+    echo -e "\n${GRAY}Pulling ${LAB}...${NO_COLOR}"
 
-  git pull
+    cd $LAB
+    git pull origin master
+  else
+    echo -e "\n${GRAY}Cloning ${LAB}...${NO_COLOR}"
+
+    git clone "git@github.com:wbotelhos/${LAB}.git"
+  fi
 done
