1d23079a751e43cf11c1f22405ad689d9e5b479d
Washington Botelho <wbotelhos@gmail.com>
2014-01-07 19:23:49 -0200

    update: using scope.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
   before_filter :require_login
 
   def index
-    @drafts = Article.select('id, published_at, slug, title').drafts.by_created
+    @drafts   = Article.home_selected.drafts.by_created
   end
 end
