f2b33ca4205b512f93e8f39a36635b7ddc022a5b
Washington Botelho <wbotelhos@gmail.com>
2014-06-12 18:09:55 -0300

    minor

--- config/initializers/slug.rb
+++ config/initializers/slug.rb
@@ -4,16 +4,17 @@ class String
   def slug
     return '' if self.blank?
 
-    from  = 'áàãâäèéêëìíîïõòóôöùúûüç'
-    to    = 'aaaaaeeeeiiiiooooouuuuc'
+    from = 'áàãâäèéêëìíîïõòóôöùúûüç'
+    to   = 'aaaaaeeeeiiiiooooouuuuc'
 
     slug = self.downcase
 
-    slug.gsub!  /\s-\s/,  '-'
-    slug.gsub!  /\s/,     '-'
-    slug.tr!    from,     to
-    slug.gsub!  /[^\w-]/, ''
-    slug.gsub!  /-$/,     ''
+    slug.gsub! /\s-\s/  , '-'
+    slug.gsub! /\s/     , '-'
+    slug.tr!   from     , to
+    slug.gsub! /[^\w-]/ , ''
+    slug.gsub! /-$/     , ''
+    slug.gsub! /ˆ-/     , ''
 
     slug
   end
