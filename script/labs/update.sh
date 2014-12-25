cf6cc62a3bf800a36a8270948780dcfe85c6ad49
Washington Botelho <wbotelhos@gmail.com>
2014-12-25 15:07:02 -0200

    add: info about clone or pull.

--- script/labs/update.sh
+++ script/labs/update.sh
@@ -29,10 +29,14 @@ pull() {
 
   while read slug; do
     if [ -d $slug ]; then
+      echo -e "\n${GRAY}Pulling ${slug}...${NO_COLOR}"
+
       cd ~/workspace/${slug}
       git pull origin master
       cd -
     else
+      echo -e "\n${GRAY}Cloning ${slug}...${NO_COLOR}"
+
       git clone "git@github.com:wbotelhos/${slug}.git"
     fi
   done < ~/workspace/blogy/script/labs/slugs.txt
