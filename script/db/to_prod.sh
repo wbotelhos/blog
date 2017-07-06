302d9f044f3aa688978be62b96aa6f4786c46dac
Washington Botelho <wbotelhos@gmail.com>
2017-07-06 03:12:54 -0300

    using blogy name on dump

--- script/db/to_prod.sh
+++ script/db/to_prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CREDENTIALS='user@wbotelhos.com'
-DATABASE='wbotelhos'
-FILE_SQL="wbotelhos_dev_`date +%Y-%m-%d_%H-%M-%S`.sql"
+DATABASE='blogy'
+FILE_SQL="blogy_development_`date +%Y-%m-%d_%H-%M-%S`.sql"
 FILE_TAR="${FILE_SQL}.tar.gz"
-SSH_KEY=~/.ssh/blog
+SSH_KEY=~/.ssh/id_rsa
 USERNAME='root'
 
 read -sp 'Database production password: ' PASSWORD
@@ -17,4 +17,3 @@ cd /tmp && mysqldump -u $USERNAME ${DATABASE}_development > $FILE_SQL && tar zcf
 ssh -i $SSH_KEY $CREDENTIALS "cd /tmp && mysqldump -u $USERNAME -p'${PASSWORD}' ${DATABASE}_production > $FILE_SQL && tar zcf $FILE_TAR $FILE_SQL" && \
 scp -i $SSH_KEY /tmp/${FILE_TAR} $CREDENTIALS:/tmp && \
 ssh -i $SSH_KEY $CREDENTIALS "cd /tmp && tar zxf $FILE_TAR && mysql -u $USERNAME -p'${PASSWORD}' ${DATABASE}_production < ${FILE_SQL}"
-
