c3753bee34f1320b01dd7b3c8bfa42b6adba80f4
Washington Botelho <wbotelhos@gmail.com>
2017-07-18 03:56:17 -0300

    some clean up

--- config/initializers/aitch.rb
+++
@@ -1,5 +0,0 @@
-# Aitch.configure do |config|
-#   config.follow_redirect = true
-#   config.redirect_limit  = 5
-#   config.timeout         = 30
-# end
--- script/labs/update.sh
+++ script/labs/update.sh
@@ -1,53 +1,15 @@
-#!/bin/bash
+cd ~/workspace
 
-##########################
-# --- Configurations --- #
-##########################
+while read slug; do
+  if [ -d $slug ]; then
+    echo -e "\n${GRAY}Pulling ${slug}...${NO_COLOR}"
 
-GRAY='\033[0;36m'
-GREEN='\033[0;32m'
-NO_COLOR='\033[1;0m'
+    cd ~/workspace/${slug}
+    git pull origin master
+    cd -
+  else
+    echo -e "\n${GRAY}Cloning ${slug}...${NO_COLOR}"
 
-JOB_NAME='Labs#update'
-
-#####################
-# --- Functions --- #
-#####################
-
-begin() {
-  echo -e "-------------------------------------"
-  echo -e "${GRAY}Starting ${JOB_NAME}...${NO_COLOR}\n"
-}
-
-end() {
-  echo -e "${GREEN}Done!${NO_COLOR}"
-  echo -e "-------------------------------------\n"
-}
-
-pull() {
-  cd ~/workspace
-
-  while read slug; do
-    if [ -d $slug ]; then
-      echo -e "\n${GRAY}Pulling ${slug}...${NO_COLOR}"
-
-      cd ~/workspace/${slug}
-      git pull origin master
-      cd -
-    else
-      echo -e "\n${GRAY}Cloning ${slug}...${NO_COLOR}"
-
-      git clone "git@github.com:wbotelhos/${slug}.git"
-    fi
-  done < ~/workspace/blogy/script/labs/slugs.txt
-}
-
-#####################
-# ---- Install ---- #
-#####################
-
-begin
-
-pull
-
-end
+    git clone "git@github.com:wbotelhos/${slug}.git"
+  fi
+done < ~/workspace/blogy/script/labs/slugs.txt
