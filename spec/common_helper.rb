e6e9e4ed42f8bc3071d2052bdb74cedf6b54c3b2
Washington Botelho <wbotelhos@gmail.com>
2020-10-20 13:44:44 -0300

    up: spec methods

---
+++ spec/common_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+
+require 'support/common'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,1 @@
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.filter_run :focus
-
-  config.order                            = :random
-  config.run_all_when_everything_filtered = true
-end
+# frozen_string_literal: true
