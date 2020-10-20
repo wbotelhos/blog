e22080ebad1f35b30f716caeb93cd74eac74b7d9
Washington Botelho <wbotelhos@gmail.com>
2020-10-20 12:52:16 -0300

    fix: missing manifest

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,11 @@
+//= link_tree ../images
+
+//= link admin.css
+//= link application.css
+//= link labs.css
+//= link session.css
+
+//= link admin.js
+//= link application.js
+//= link labs.js
+//= link session.js
