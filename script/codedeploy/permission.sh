2600cbba223bf825a78c6b37b67d63467d423699
Washington Botelho <wbotelhos@gmail.com>
2020-10-20 22:23:28 -0300

    rm: codedeploy files

--- script/codedeploy/permission.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chown -R deploy:www-data /var/www
-chown -R deploy:www-data /usr/local/ruby/gem
--- script/codedeploy/symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATE=$(date "+%Y%m%d%H%M%S")
-
-mkdir -p /var/www/blog/releases
-
-mv /var/www/blog/codedeploy /var/www/blog/releases/${DATE}
-
-ln -nfs /var/www/blog/releases/${DATE} /var/www/blog/current
