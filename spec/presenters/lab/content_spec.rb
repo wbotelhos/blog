efdf7a2852908dcca4ca1a223c94b90e5d0c916d
Washington Botelho <wbotelhos@gmail.com>
2020-10-31 12:23:10 -0300

    spec: fix it to lab

--- spec/presenters/lab/content_spec.rb
+++ spec/presenters/lab/content_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-RSpec.describe ArticlePresenter, '#content' do
-  let!(:article) { FactoryBot.create(:article, body: 'body') }
+RSpec.describe LabPresenter, '#content' do
+  let!(:lab) { FactoryBot.create(:lab, body: 'body') }
 
   before { allow(HighlighterService).to receive(:highlight).with('body').and_return('content') }
 
   it 'receives text processed by Highlighter service' do
-    expect(described_class.new(article).content).to eq('content')
+    expect(described_class.new(lab).content).to eq('content')
   end
 end
